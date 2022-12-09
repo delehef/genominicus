9e6d1bc0c173af99aa03052fd46b10fc348c4213
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-12-09 12:22:48 +0100

    Query on gene

--- src/utils.rs
+++ src/utils.rs
@@ -9,7 +9,7 @@ use std::iter::FromIterator;
 use svarog::*;
 
 const ANCESTRAL_QUERY: &str = concat!(
-    "select gene, ancestral, species, chr, start, direction, left_tail_names, right_tail_names from genomes where protein=?",
+    "select gene, ancestral, species, chr, start, direction, left_tail_names, right_tail_names from genomes where gene=?",
 );
 const LEFTS_QUERY: &str = "select ancestral, direction from genomes where species=? and chr=? and start<? order by start desc limit ?";
 const RIGHTS_QUERY: &str = "select ancestral, direction from genomes where species=? and chr=? and start>? order by start asc limit ?";
